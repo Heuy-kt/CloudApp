cbbbe7a799299d0b5c6596a7b3b1909aaaec6db6
7devanle <ffamexyz@gmail.com>
2025-01-23 18:19:46 +0100

    supposed first commit

---
+++ EurekaServer/src/test/java/Dot/kt/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Dot.kt.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ServiceB/src/test/java/Dot/kt/ServiceB/ServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package Dot.kt.ServiceB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
